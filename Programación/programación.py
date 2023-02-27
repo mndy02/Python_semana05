d3bb5828dc24222351cfa2f313df82d5b303346f
mndy02 <126424519+mndy02@users.noreply.github.com>
2023-02-26 18:24:49 -0600

    Add files via upload
    Hola Ing. Jonathan, no logramos completar el equipo pero trabajamos acorde a las instrucciones de la actividad.

---
+++ Programación/programación.py
@@ -0,0 +1,82 @@
+print("Buenas tardes Ing. Jonathan, somos estudiantes de la clase de Fundamentos de programación, este es nuestro trabajo")
+print("")
+print("Integrantes del equipo:")
+print("")
+y = "Integrante 1"
+print(y)
+nombreUno = "Amanda"
+nombreDos = "Yurdia"
+apellidoUno = "Portillo"
+apellidoDos = "Marroquín"
+print(nombreUno, nombreDos, apellidoUno, apellidoDos)
+sex = "Sexo: Femenino"
+print(sex)
+x = "Edad: 17 años"
+print(x)
+edadUno = 17
+carrera = 5
+edadEgre = edadUno + carrera
+print("La edad de Amanda al egresar de la carrera será de: ", edadEgre),print("años")
+print("")
+print("Asignación de cadenas de caracteres")
+mensaje = "Hola soy Amanda, tengo 17 años"
+buscarSubcadena = mensaje.find("Amanda")
+print(buscarSubcadena)
+print("")
+k = "Integrante 2"
+print(k)
+nombreUno = "Kenia"
+nombreDos = "Guadalupe"
+apellidoUno = "Figueroa"
+apellidoDos = "Ortega"
+print(nombreUno, nombreDos, apellidoUno, apellidoDos)
+sex = "Sexo: Femenino"
+print(sex)
+x = "Edad: 17 años"
+print(x)
+edadDos = 17
+carrera = 5
+edadEgre = edadDos + carrera
+print("La edad de Kenia al egresar de la carrera será de: ", edadEgre),print("años")
+print("")
+b = "Integrante 3"
+print(b)
+nombreUno = "Brandon"
+nombreDos = "Josué"
+apellidoUno = "Rivera"
+apellidoDos = "Alemán"
+print(nombreUno, nombreDos, apellidoUno, apellidoDos)
+sex = "Sexo: Masculino"
+print(sex)
+x = "Edad: 19 años"
+print(x)
+edadTres = 19
+carrera = 5
+edadEgre = edadTres + carrera
+print("La edad de Brandon al egresar de la carrera será de: ", edadEgre),print("años")
+print("")
+w = "Integrante 4"
+print(w)
+nombreUno = "Wendy"
+nombreDos = "Marisol"
+apellidoUno = "Espinoza"
+apellidoDos = "Rivas"
+print(nombreUno, nombreDos, apellidoUno, apellidoDos)
+sex = "Sexo: Femenino"
+print(sex)
+x = "Edad: 16 años"
+print(x)
+edadCuatro = 16
+carrera = 5
+edadEgre = edadCuatro + carrera
+print("La edad de Wendy al egresar de la carrera sería de: ", edadEgre), print("años")
+print("")
+print("Promedio de edad de los integrantes del equipo")
+edadUno = 17
+edadDos = 17
+edadTres = 19
+edadCuatro = 16
+dividir = 4
+sumaEdad = edadUno + edadDos + edadTres + edadCuatro
+print("Suma de edades:", sumaEdad)
+print("El promedio de edad del equipo es:", sumaEdad / dividir)
